c23e1ed98b7516848d603be6ce14871334878a2a
djpukatsv <djpuka7@gmail.com>
2026-04-21 08:45:21 +1000

    Update ListPropertyPage.jsx

--- client/src/pages/ListPropertyPage.jsx
+++ client/src/pages/ListPropertyPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function ListPropertyPage({ navigate, user }) {
   const [form, setForm] = useState({
